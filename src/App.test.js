cc659caceac785b90a6b4fcd9d4dba07a742ea57
Tsuyoshi Miyake <tsuyoshi.miyake@gmail.com>
2020-05-13 21:09:28 +0900

    intentionally failing test again

--- src/App.test.js
+++ src/App.test.js
@@ -8,6 +8,6 @@ it('renders without crashing', () => {
   ReactDOM.unmountComponentAtNode(div);
 });
 
-// it('should fail', () => {
-//  expect(1).toEqual(2);
-// })
+it('should fail', () => {
+  expect(1).toEqual(2);
+})
